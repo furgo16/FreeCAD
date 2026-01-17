cbbdb1270f71808ea648f1b62e035a2be35c0924
Furgo <148809153+furgo16@users.noreply.github.com>
2026-01-17 06:29:56 +0100

    BIM: create scaffolding for the ArchCovering object

--- src/Mod/BIM/bimcommands/BimCovering.py
+++ src/Mod/BIM/bimcommands/BimCovering.py
@@ -11,3 +11,47 @@ This command creates an Arch Covering object, used to represent surface finishes
 cladding, or ceiling tiles. It opens a task panel allowing the user to select a base face or object
 and configure parameters for solid tiles, parametric patterns, or standard hatch patterns.
 """
+
+import FreeCAD
+
+if FreeCAD.GuiUp:
+    from PySide import QtCore, QtGui
+    import FreeCADGui
+
+    QT_TRANSLATE_NOOP = FreeCAD.Qt.QT_TRANSLATE_NOOP
+    translate = FreeCAD.Qt.translate
+else:
+
+    def translate(ctxt, txt):
+        return txt
+
+    def QT_TRANSLATE_NOOP(ctxt, txt):
+        return txt
+
+
+class BIM_Covering:
+    def GetResources(self):
+        return {
+            "Pixmap": "BIM_Covering",
+            "MenuText": QT_TRANSLATE_NOOP("BIM_Covering", "Covering"),
+            "ToolTip": QT_TRANSLATE_NOOP(
+                "BIM_Covering", "Creates a covering (floor finish, cladding) on a selected face"
+            ),
+            "Accel": "C, V",
+        }
+
+    def IsActive(self):
+        return hasattr(FreeCADGui.getMainWindow().getActiveWindow(), "getSceneGraph")
+
+    def Activated(self):
+        self.doc = FreeCAD.ActiveDocument
+        self.view = FreeCADGui.ActiveDocument.ActiveView
+
+        # Show Task Panel
+        import ArchCovering
+
+        self.task = ArchCovering.ArchCoveringTaskPanel(command=self)
+        FreeCADGui.Control.showDialog(self.task)
+
+    def finish(self):
+        FreeCADGui.Control.closeDialog()
